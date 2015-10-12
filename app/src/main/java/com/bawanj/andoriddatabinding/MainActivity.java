9d6add899ca4bac2923905a1991ce76c628bec36
Jeffrey Chou <sfatdisk@gmail.com>
2015-10-12 13:01:28 -0400

    Initial commit

---
+++ app/src/main/java/com/bawanj/andoriddatabinding/MainActivity.java
@@ -0,0 +1,48 @@
+package com.bawanj.andoriddatabinding;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.bawanj.andoriddatabinding.databinding.ActivityMainBinding;
+import com.bawanj.andoriddatabinding.model.User;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // ViewModel: ActivityMainBinding <= activity_main.xml
+        ActivityMainBinding mainBinding
+                = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        User user= new User("Jeffrey", "Nyu168168168");
+        mainBinding.setUser(user);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/bawanj/andoriddatabinding/model/User.java
@@ -0,0 +1,29 @@
+package com.bawanj.andoriddatabinding.model;
+
+
+public class User {
+
+    private String userName;
+    private String userPassword;
+
+    public User(String userName, String userPassword) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
